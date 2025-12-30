5f6e850314b0413e30a4a380e36190fcabb66f06
ajdeashhh <163972288+ajdeashhh@users.noreply.github.com>
2025-12-30 18:48:23 +0800

    Add files via upload

---
+++ frontend/src/pages/student/layout.tsx
@@ -0,0 +1,26 @@
+
+import type { Metadata } from 'next';
+import './globals.css';
+import { Toaster } from '@/components/ui/toaster';
+
+export const metadata: Metadata = {
+  title: 'Claro M. Recto High School',
+  description: 'LMS for Claro M. Recto High School',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+      </head>
+      <body className="font-body antialiased">
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
