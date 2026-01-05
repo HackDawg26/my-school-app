627dcc1ac9aa072b5d7880d1ab33ef7dab5be9c2
Eishiru <cagubatej@yahoo.com>
2026-01-05 17:27:02 +0800

    Reset

--- frontend/src/pages/auth/Unauthorized.tsx
+++
@@ -1,7 +0,0 @@
-
-
-export const Unauthorized = () => {
-  return (
-    <div>Unauthorized</div>
-  )
-}
--- frontend/src/pages/student/gradebook/Gradebook.tsx
+++
@@ -1,7 +0,0 @@
-
-
-export const Gradebook = () => {
-  return (
-    <div>Gradebook</div>
-  )
-}
